e7f20cf28cccd7c0de5fdd3a72531ddc45d0442e
Jonathan H <hurd.jonathan@gmail.com>
2023-10-02 13:38:01 -0500

    refactored script template to use Handlebars template for new item form

--- lesson_5/inventory_order_form_start_code/javascripts/inventory.js
+++ lesson_5/inventory_order_form_start_code/javascripts/inventory.js
@@ -1,17 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+
 var inventory;
 
 (function() {
   inventory = {
     lastId: 0,
     collection: [],
+
     setDate: function() {
       var date = new Date();
       $("#order_date").text(date.toUTCString());
     },
+
     cacheTemplate: function() {
-      var $iTmpl = $("#inventory_item").remove();
-      this.template = $iTmpl.html();
+      var iTmpl = document.querySelector("#inventory_item");
+      this.template = Handlebars.compile(iTmpl.innerHTML);
     },
+
     add: function() {
       this.lastId++;
       var item = {
@@ -24,11 +29,13 @@ var inventory;
 
       return item;
     },
+
     remove: function(idx) {
       this.collection = this.collection.filter(function(item) {
         return item.id !== idx;
       });
     },
+
     get: function(id) {
       var found_item;
 
@@ -41,49 +48,58 @@ var inventory;
 
       return found_item;
     },
+
     update: function($item) {
       var id = this.findID($item),
-          item = this.get(id);
+        item = this.get(id);
 
       item.name = $item.find("[name^=item_name]").val();
       item.stock_number = $item.find("[name^=item_stock_number]").val();
       item.quantity = $item.find("[name^=item_quantity]").val();
     },
+
     newItem: function(e) {
       e.preventDefault();
-      var item = this.add(),
-          $item = $(this.template.replace(/ID/g, item.id));
-
-      $("#inventory").append($item);
+      var item = this.add()
+      let inventory = document.querySelector("#inventory tbody");
+      inventory.insertAdjacentHTML('beforeend', this.template(item).trim());
     },
+
     findParent: function(e) {
       return $(e.target).closest("tr");
     },
+
     findID: function($item) {
       return +$item.find("input[type=hidden]").val();
     },
+
     deleteItem: function(e) {
       e.preventDefault();
       var $item = this.findParent(e).remove();
 
       this.remove(this.findID($item));
     },
+
     updateItem: function(e) {
       var $item = this.findParent(e);
 
       this.update($item);
     },
+
     bindEvents: function() {
       $("#add_item").on("click", $.proxy(this.newItem, this));
       $("#inventory").on("click", "a.delete", $.proxy(this.deleteItem, this));
       $("#inventory").on("blur", ":input", $.proxy(this.updateItem, this));
     },
+
     init: function() {
       this.setDate();
-      this.cacheTemplate();
+      this.cacheTemplate(); //replace with handlebars
       this.bindEvents();
     }
   };
 })();
 
 $($.proxy(inventory.init, inventory));
+
+});
