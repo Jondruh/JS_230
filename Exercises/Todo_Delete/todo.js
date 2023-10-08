1251cd24c41ccdac92409ba134bb64c99e6b1084
Jonathan H <hurd.jonathan@gmail.com>
2023-10-08 09:46:42 -0500

    added context menu for right clicking on todo items

--- Exercises/Todo_Delete/todo.js
+++ Exercises/Todo_Delete/todo.js
@@ -1,4 +1,4 @@
-todo_items = [
+todoItems = [
   { id: 1, title: 'Homework' },
   { id: 2, title: 'Shopping' },
   { id: 3, title: 'Calling Mom' },
@@ -9,58 +9,114 @@ todo_items = [
 document.addEventListener("DOMContentLoaded", () => {
   let todoList = document.querySelector('#todos');
   let deleteDialog = document.querySelector('#delete-dialog');
+  let contextMenu = document.querySelector('#context-menu');
+  let contextDelete = document.querySelector('#context-delete');
+  let menuItems = contextMenu.querySelectorAll('button');
 
-  todo_items.forEach(item => {
-    let id = String(item.id);
-    let title = String(item.title);
+  setupContextMenu();
+  setupTodos();
+  setupDeleteButton();
+  setupDeleteDialog();
 
-    let todoTemplate = `
-    <li id=${escape(id)}>
-      <div class="title">${escape(title)}</div>
-      <button class='delete'>Delete</button>
-    </li>
-    `
+  function setupDeleteButton() {
+    todoList.addEventListener('click', event => {
+      if (event.target.tagName === 'BUTTON') {
+        let message = deleteDialog.firstElementChild;
+        let title = event.target.parentElement.querySelector('.title').innerText;
+        let todoId = event.target.parentElement.id;
 
-    todoList.insertAdjacentHTML('beforeend', todoTemplate);
-  });
+        message.innerText = `Are you sure you'd like to delete '${title}'?` 
+        deleteDialog.setAttribute("data-id", todoId)
+        
+        deleteDialog.className = 'show';
+      }
+    });
+  }
 
-  todoList.addEventListener('click', event => {
-    if (event.target.tagName === 'BUTTON') {
+  function setupContextMenu() {
+    todoList.addEventListener('contextmenu', e => {
+      if (['DIV', 'BUTTON'].includes(e.target.tagName)) {
+        e.preventDefault();
+  
+        contextMenu.dataset.todoId = e.target.parentElement.id;
+        contextMenu.dataset.todoTitle = e.target.innerText;
+        contextMenu.className = 'show';
+        contextMenu.style.left = e.pageX + 'px';
+        contextMenu.style.top = e.pageY + 'px';
+      }
+    });
+  
+    contextDelete.addEventListener('click', e => {
       let message = deleteDialog.firstElementChild;
-      let title = event.target.parentElement.querySelector('.title').innerText;
-      let todoId = event.target.parentElement.id;
+      let title = contextMenu.dataset.todoTitle;
+      let todoId = contextMenu.dataset.todoId;
 
       message.innerText = `Are you sure you'd like to delete '${title}'?` 
       deleteDialog.setAttribute("data-id", todoId)
       
       deleteDialog.className = 'show';
-    }
-  });
+    });
 
-  let deleteNoButton = document.querySelector('#no');
-  let deleteYesButton = document.querySelector('#yes');
+    document.addEventListener('click', e => {
+      contextMenu.className = 'hide';
+    });
 
-  deleteNoButton.addEventListener('click', e => {
-    deleteDialog.className = 'hide'; 
-  });
+    setupMenuHighlights(menuItems);
+  }
 
-  deleteYesButton.addEventListener('click', e => {
-    let todoId = e.target.parentElement.dataset.id;
-    deleteDialog.className = 'hide'; 
-    deleteItem(todoId);
-  })
+  function setupDeleteDialog() {
+    let deleteNoButton = document.querySelector('#no');
+    let deleteYesButton = document.querySelector('#yes');
   
+    deleteNoButton.addEventListener('click', e => {
+      deleteDialog.className = 'hide'; 
+    });
+  
+    deleteYesButton.addEventListener('click', e => {
+      let todoId = e.target.parentElement.dataset.id;
+      deleteDialog.className = 'hide'; 
+      deleteItem(todoId);
+    })
+  }
+  
+  function setupTodos() {
+    todoItems.forEach(item => {
+      let id = String(item.id);
+      let title = String(item.title);
+  
+      let todoTemplate = `
+      <li id=${escape(id)}>
+        <div class="title">${escape(title)}</div>
+        <button class='delete'>Delete</button>
+      </li>
+      `
+  
+      todoList.insertAdjacentHTML('beforeend', todoTemplate);
+    });
+  }
+  
+  function deleteItem(itemId) {
+    document.getElementById(itemId).remove()
+  
+    let itemIndex = todoItems.find(todo => todo.id === itemId);
+    todoItems.splice(itemIndex, 1);
+  }
+  
+  function escape(string) {
+    return string.replace(/[^0-9a-zA-Z]/g, char => {
+      return "&#" + char.charCodeAt(0) + ";";
+    });
+  }
+  
+  function setupMenuHighlights() {
+    menuItems.forEach(button => {
+      button.addEventListener('mouseenter', e => {
+        e.target.style.backgroundColor = 'var(--menu-highlight)';
+      });
+  
+      button.addEventListener('mouseleave', e => {
+        e.target.style.backgroundColor = 'var(--menu-background)';
+      });
+    });
+  }
 });
-
-function deleteItem(itemId) {
-  document.getElementById(itemId).remove()
-
-  let itemIndex = todo_items.find(todo => todo.id === itemId);
-  todo_items.splice(itemIndex, 1);
-}
-
-function escape(string) {
-  return string.replace(/[^0-9a-zA-Z]/g, char => {
-    return "&#" + char.charCodeAt(0) + ";";
-  });
-}
