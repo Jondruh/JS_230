f55706507638e65b1490d9a9f515a28cc957d3c5
Jonathan H <hurd.jonathan@gmail.com>
2023-10-09 13:41:30 -0500

    Cars display and are can be filtered.

--- Exercises/Cars/cars.js
+++ Exercises/Cars/cars.js
@@ -8,6 +8,75 @@ const cars = [
   { make: 'Audi', image: 'images/audi-a4-2013.jpg', model: 'A4', year: 2013, price: 26000 },
 ];
 
-document.addEventListener("DOMContentLoaded", () => {
+$(function() {
+  fillFilterSelectors();
+  displayCars(cars);
 
+  $('#filter-controls').on('submit', event => {
+    event.preventDefault();
+    let data = new FormData(event.target);
+    let filters = {};
+
+    data.entries().forEach(category => {
+      if (category[1] !== 'all') {
+        filters[category[0]] = category[1];
+      }
+    })
+
+    displayCars(filterCars(cars, filters));
+  });
 });
+
+function filterCars(cars, filters) {
+  let filteredCars = cars;
+
+  Object.keys(filters).forEach(category => {
+    filteredCars = filteredCars.filter(car => {
+      return String(car[category]) === filters[category];
+    });
+  });
+
+  console.log(filteredCars);
+  return filteredCars; 
+}
+
+function displayCars(cars) {
+  let $car = $('#car-template');
+  let $carsContainer = $('#cars-container');
+  $carsContainer.empty();
+
+  let carTemplate = Handlebars.compile($car.html());
+
+  cars.forEach(car => {
+    $carsContainer.append(carTemplate(car));
+  });
+}
+
+function fillFilterSelectors() {
+  let uniqueCars = { make: [], model: [], year: [], price: [] }
+
+  Object.keys(cars[0]).forEach(category => {
+    if(category !== 'image') {
+      for(let i = 0; i < cars.length; i += 1) {
+        if (!uniqueCars[category].includes(cars[i][category])) {
+          uniqueCars[category].push(cars[i][category]);
+        }
+      }
+    }
+  });
+
+  Object.keys(uniqueCars).forEach(category => {
+    let $selectElement = $(`#${category}-select`)
+
+    uniqueCars[category].forEach(selectOption => {
+      let option = document.createElement('option');
+
+      option.setAttribute('value', selectOption);
+      option.innerText = selectOption;
+
+      $selectElement.append(option);
+    });
+    
+  });
+
+}
