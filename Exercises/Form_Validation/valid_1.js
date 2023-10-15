2994e48c61833359f7f5b31ea94bb99edf06a1de
Jonathan H <hurd.jonathan@gmail.com>
2023-10-15 10:30:51 -0500

    Added credit card form and auto focus next

--- Exercises/Form_Validation/valid_1.js
+++ Exercises/Form_Validation/valid_1.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let phone = document.querySelector('#phone');
   let form = document.querySelector('#signup-form');
   let formError = document.querySelector('div.form-error-message');
+  let credits = document.querySelectorAll('.credit-input');
 
   form.addEventListener('submit', e => {
     if (!form.checkValidity()) {
@@ -14,9 +15,53 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  [firstName, lastName].forEach(input => {
+    input.addEventListener('beforeinput', e => {
+      if (e.inputType === 'insertText' && (!e.data.match(/[a-zA-Z'\s]/))) {
+          e.preventDefault();
+      }
+    });
+  });
+
+  credits.forEach(input => {
+    input.addEventListener('beforeinput', e => {
+      if (e.inputType === 'insertText' && (!e.data.match(/[0-9]/))) {
+        e.preventDefault();
+      }
+      
+      if (e.inputType === 'insertText' && e.target.value.length >= 4) {
+        e.preventDefault();
+      }
+    });
+
+    input.addEventListener('blur', e => {
+      if (e.target.validity.valid) {
+        e.target.classList.remove('invalid');
+        
+        if (Array.from(credits).every(input => input.validity.valid)) {
+          e.target.parentElement.nextElementSibling.classList.remove('invalid');
+        }
+      } else {
+        e.target.classList.add('invalid');
+        e.target.parentElement.nextElementSibling.classList.add('invalid');
+      }
+
+      if (form.checkValidity()) {
+        formError.classList.remove('invalid');
+      }
+    });
+  });
+
+  Array.from(credits).slice(0, -1).forEach(input => {
+    input.addEventListener('keyup', e => {
+      if (e.target.value.length === 4) {
+        e.target.nextElementSibling.focus();
+      }
+    });
+  });
+
   [firstName, lastName, email, password, phone].forEach(input => {
     input.addEventListener('blur', e => {
-      console.log(e.target.validity.valid);
 
       if (e.target.validity.valid) {
         e.target.classList.remove('invalid');
